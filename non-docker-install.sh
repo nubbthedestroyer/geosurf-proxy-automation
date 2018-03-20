7825db7c9baef5292523501b8636d79a147de1f4
nubbthedestroyer <michael.lucas@rackspace.com>
2018-03-19 23:15:22 -0500

    fix non-docker install

--- non-docker-install.sh
+++ non-docker-install.sh
@@ -13,6 +13,7 @@ rm -rf /etc/squid3/users
 
 sudo cp etc/sarg/* /etc/sarg
 sudo cp etc/squid/* /etc/squid
+mkdir -p /etc/squid3
 sudo cp etc/squid3/* /etc/squid3
 
 sudo systemctl restart squid.service
